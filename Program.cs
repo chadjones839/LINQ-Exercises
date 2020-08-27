7193926b295d1c6d9b3e09b7163b21815d795ca5
Chad Jones <chadjones839@gmail.com>
2020-08-27 15:11:03 -0500

    Initial Files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RESTRICTION/FILTERING OPERATIONS
+            
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits where fruit.StartsWith("L") select fruit;
+
+            Console.WriteLine("Fruits that start with the letter L");
+            foreach (string LFruit in LFruits)
+            {
+                Console.WriteLine($"{LFruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where();
+        }
+    }
+}
