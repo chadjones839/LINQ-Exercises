c1e2de837edf68379b4c0c46d182e7ff8deee775
Chad Jones <chadjones839@gmail.com>
2020-08-27 15:57:20 -0500

    order by

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace linq
         static void Main(string[] args)
         {
             //RESTRICTION/FILTERING OPERATIONS
-            
+
             // Find the words in the collection that start with the letter 'L'
             List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
@@ -40,7 +40,56 @@ namespace linq
                 96
             };
 
-            IEnumerable<int> fourSixMultiples = numbers.Where();
+            List<int> fourMultiples = new List<int>();
+            List<int> sixMultiples = new List<int>();
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 6 == 0 || number % 4 == 0);
+
+            Console.WriteLine();
+            Console.WriteLine("Multiples of 4 and 6");
+
+            foreach (int number in fourSixMultiples)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather",
+                "James",
+                "Xavier",
+                "Michelle",
+                "Brian",
+                "Nina",
+                "Kathleen",
+                "Sophia",
+                "Amir",
+                "Douglas",
+                "Zarley",
+                "Beatrice",
+                "Theodora",
+                "William",
+                "Svetlana",
+                "Charisse",
+                "Yolanda",
+                "Gregorio",
+                "Jean-Paul",
+                "Evangelina",
+                "Viktor",
+                "Jacqueline",
+                "Francisco",
+                "Tre"
+            };
+
+            List<string> descend = names.OrderByDescending(name => name).ToList();
+
+            Console.WriteLine();
+            Console.WriteLine("NAMES IN DESCENDING ORDER");
+            foreach (string name in descend)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
